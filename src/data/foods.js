57d139f54c9fec9536517196c13300906ca5a4bf
ezharrison12-beep <ezharrison12@gmail.com>
2026-08-24 12:01:40 -0700

    Add starter food database for meal logging

---
+++ src/data/foods.js
@@ -0,0 +1,14 @@
+export const FOODS = [
+  { id: 'chicken-breast', name: 'Chicken breast', serving: '100 g', calories: 165, protein: 31, carbs: 0, fat: 3.6 },
+  { id: 'rice', name: 'White rice, cooked', serving: '1 cup', calories: 205, protein: 4.3, carbs: 44.5, fat: 0.4 },
+  { id: 'eggs', name: 'Whole eggs', serving: '2 large', calories: 144, protein: 12.6, carbs: 0.7, fat: 9.5 },
+  { id: 'oats', name: 'Oatmeal, cooked', serving: '1 cup', calories: 158, protein: 5.9, carbs: 27, fat: 3.2 },
+  { id: 'greek-yogurt', name: 'Greek yogurt, nonfat', serving: '170 g', calories: 100, protein: 17, carbs: 6, fat: 0 },
+  { id: 'banana', name: 'Banana', serving: '1 medium', calories: 105, protein: 1.3, carbs: 27, fat: 0.4 },
+  { id: 'avocado', name: 'Avocado', serving: '1/2 fruit', calories: 120, protein: 1.5, carbs: 6.5, fat: 11 },
+  { id: 'salmon', name: 'Atlantic salmon', serving: '100 g', calories: 208, protein: 20, carbs: 0, fat: 13 },
+  { id: 'sweet-potato', name: 'Sweet potato', serving: '1 medium', calories: 103, protein: 2.3, carbs: 24, fat: 0.1 },
+  { id: 'whey', name: 'Whey protein', serving: '1 scoop', calories: 120, protein: 24, carbs: 3, fat: 2 },
+  { id: 'peanut-butter', name: 'Peanut butter', serving: '2 tbsp', calories: 190, protein: 7, carbs: 7, fat: 16 },
+  { id: 'broccoli', name: 'Broccoli', serving: '1 cup', calories: 55, protein: 3.7, carbs: 11.2, fat: 0.6 }
+];
